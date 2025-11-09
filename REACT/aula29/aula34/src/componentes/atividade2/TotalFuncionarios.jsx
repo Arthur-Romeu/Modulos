21b234517c2fbf4e6c931284d481828dffb00099
Arthur Romeu <arthurromeu76@gmail.com>
2025-11-08 21:25:33 -0300

    Pastas do módulo de REACT

--- REACT/aula29/aula34/src/componentes/atividade2/TotalFuncionarios.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { ContextoFuncionarios } from "./ContextoFuncionarios";
-
-export default function TotalFuncionarios() {
-    const {funcionarios} = useContext(ContextoFuncionarios) // desestruturando o objetop do contexto para uma variavel
-
-    return(<h2>A empresa conta com {funcionarios.length} funcionários</h2>)
-}
